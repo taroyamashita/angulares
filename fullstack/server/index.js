a6862c30f60bcbc039919f3a3921f753adfc8ca4
Taro yamashita <taro.c.yamashita@gmail.com>
2018-11-15 16:34:41 -0500

    added an aditional project

--- fullstack/server/index.js
+++ fullstack/server/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const firebase = require('firebase');
+
+var config = {
+    apiKey: "AIzaSyBsAY2bthJD9-UT-6XkgzR_1RQ5q47JGvU",
+    authDomain: "angulares-f168e.firebaseapp.com",
+    databaseURL: "https://angulares-f168e.firebaseio.com",
+    projectId: "angulares-f168e",
+    storageBucket: "angulares-f168e.appspot.com",
+    messagingSenderId: "686543903958"
+  };
 
 let testData = [  
     {  
@@ -20,6 +30,27 @@ let testData = [
        "Tags":"Tags for Project1"
     }
  ]
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+const settings = { timestampsInSnapshots: true};
+db.settings(settings);
+
+
+//console.log(db);
+
+db.collection('projects').get().then((data)=>{
+   data.docs.forEach(doc => {
+       console.log(doc.data());
+   })
+})
+
+
+db.collection('projects').doc().set({
+    Description: "Description for Project 69",
+    ProjectTitle: "Title for Project69",
+    Tags: "Tags for Project69"
+})
 
 const PORT = process.env.PORT || 8069; 
 
