e67d2cc77e1fcfe8720dd8b03835cbcc9bd08e99
Taro yamashita <taro.c.yamashita@gmail.com>
2018-11-15 14:29:33 -0500

    connect to db

--- fullstack/src/app/app.module.ts
+++ fullstack/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DirectoryComponent } from './directory/directory.component';
--- fullstack/src/app/logging.service.ts
+++ fullstack/src/app/logging.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios'; 
 
 @Injectable()
 export class LoggingService {
   log(){
     console.log('I am a loggin service');
+    axios.get('http://localhost:8069').then((data)=>{
+      console.log(data.data);
+    }).catch((err)=>{
+      console.log(err);
+    })
   }
 
   constructor() { }
